edea4f3f6555ffd66a754c523f9d42170c52cece
jobala <japhethobalak@gmail.com>
2019-07-25 16:23:59 +0300

    Adds support for switching between Java and C#

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 
 import './App.css';
 import { Console } from './console';
 import { Editor } from './editor';
 
+const languages = {
+  java: {
+    snippet: `User user = graphClient.me()
+  .buildRequest()
+  .get();
+
+System.out.println(user.displayName);
+      `,
+    url: 'https://ms-graph-playground-backend.azurewebsites.net/api/playground/java',
+  },
+  csharp: {
+    snippet: `var user = await graphClient.Me
+  .Request()
+  .GetAsync();
+
+Console.WriteLine(user.displayName);
+      `,
+    url: 'https://ms-graph-playground-backend.azurewebsites.net/api/playground/csharp',
+  },
+};
+
 function App() {
   const [response, setResponse] = useState('');
-  const [code, setCode] = useState(`Console.WriteLine("Hello, World!");`);
+  const [code, setCode] = useState(languages.csharp.snippet);
+  const [language, setLanguage] = useState(languages.csharp);
 
-  async function handleRunCode(codeFromEditor: string) {
+  async function handleRunCode() {
     setResponse('Compiling...');
-    const resp = await fetch('https://localhost:44375/api/playground', {
+    const resp = await fetch(language.url, {
       method: 'POST',
-      body: codeFromEditor,
+      body: code,
     });
 
     const text = await resp.text();
@@ -20,7 +42,14 @@ function App() {
   }
 
   function handleSetCode(codeFromEditor: string): void {
+    console.log(codeFromEditor);
     setCode(codeFromEditor);
+  }
+
+  function handleSelect(event: any): void {
+    const lang = event.target.value;
+    setLanguage((languages as any)[lang]);
+    setCode((languages as any)[lang].snippet);
   }
 
   return (
@@ -29,7 +58,11 @@ function App() {
         <div className='toolbar'>
           <p style={{ paddingLeft: 10 }}>Code Editor</p>
           <div className='run-btn'>
-            <button onClick={() => handleRunCode(code)}>Run</button>
+            <select onChange={(event) => handleSelect(event)}>
+              <option value='csharp'>C#</option>
+              <option value='java'>Java</option>
+            </select>
+            <button onClick={() => handleRunCode()}>Run</button>
           </div>
         </div>
         <Editor onCode={handleSetCode} code={code} />
@@ -38,7 +71,7 @@ function App() {
         <div className='toolbar'>
           <p style={{ paddingLeft: 10 }}>Console</p>
         </div>
-        <Console response={response}/>
+        <Console response={response} />
       </div>
     </div>
   );
